f0245810a0fbed60d483a25eaf1e08df3ba5c597
pramithvidusara@gmail.com <43032202+TilangaPramith@users.noreply.github.com>
2020-08-21 02:57:05 +0530

    navigation

--- src/screens/Root/Root.js
+++ src/screens/Root/Root.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FeatherIcons from 'react-native-vector-icons/Feather';
 
 import Home from '../Home';
 import Bookmarks from '../Bookmarks';
@@ -11,6 +13,21 @@ const Tab = createBottomTabNavigator();
 const Root = () => {
     return (
         <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Home') {
+                iconName = "home";
+              } else if (route.name === 'Bookmarks') {
+                iconName = "bookmark";
+              }
+  
+              // You can return any component that you like here!
+              return <FeatherIcons name={iconName} size={size} color={color} />;
+            },
+          })}
+
             tabBarOptions={{
                 activeTintColor: "tomato",
                 inactiveTintColor: "gray"
